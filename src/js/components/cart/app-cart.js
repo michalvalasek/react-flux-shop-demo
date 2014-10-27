1da1e98b73f262975bc46a5b5f54225572604af0
Michal Valasek <michal.valasek@gmail.com>
2014-10-27 15:54:14 +0100

    Extract cart watch stuff into a mixin.

--- src/js/components/cart/app-cart.js
+++ src/js/components/cart/app-cart.js
@@ -5,21 +5,16 @@ var AppStore = require('../../stores/app-store.js');
 var RemoveFromCart = require('./app-removefromcart.js');
 var Increase = require('./app-increase.js');
 var Decrease = require('./app-decrease.js');
+var StoreWatchMixin = require('../../mixins/StoreWatchMixin.js');
 
 function cartItems(){
   return { items: AppStore.getCart() }
 }
 
 var Cart = React.createClass({
-  getInitialState: function() {
-    return cartItems();
-  },
-  componentWillMount: function() {
-    AppStore.addChangeListener(this._onChange);
-  },
-  _onChange: function(){
-    this.setState(cartItems()); 
-  },
+  mixins: [
+    StoreWatchMixin(cartItems)
+  ],
   render: function() {
     var total = 0;
     var items = this.state.items.map(function(item, i){
